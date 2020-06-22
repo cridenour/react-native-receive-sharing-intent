3d8cfb306fabc357754a752729c85e7c0b8a8684
Chris Ridenour <chrisridenour@gmail.com>
2020-06-21 22:02:58 -0400

    Update how we pass on JSON

--- index.js
+++ index.js
@@ -57,21 +57,21 @@ export default class ReceiveSharingIntentModule {
 
 
 const iosSortedData = (data) => {
-    let objects = { filePath: null, text: null, weblink: null, mimeType: null, contentUri: null, fileName: null, extension: null };
+    let objects = { path: null, text: null, url: null, mimeType: null, contentUri: null, fileName: null, extension: null };
     let file = data;
     try {
         let files = JSON.parse(file)
         let object = [];
         for (let i = 0; i < files.length; i++) {
-            let path = files[i].path;
-            let obj = {
-                ...objects,
-                fileName: getFileName(path),
-                extension: getExtension(path),
-                mimeType: getMimeType(path),
-                filePath: path
+            let f = files[i];
+
+            if (typeof(f.path) !== 'undefined' && f.path !== null && f.path !== '') {
+                f.fileName = getFileName(f.path);
+                f.extension = getExtension(f.path);
+                f.mimeType = getMimeType(f.path);
             }
-            object.push(obj);
+
+            object.push(f);
         }
         return object;
     } catch (error) {
